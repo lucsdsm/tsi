24aff6fce45466938d32f5d4fc8e9610bd55db97
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-04-04 09:13:56 -0300

    Adição do exercício 1 dos slides de Estruturas

---
+++ 2024.1/ilp/04.04.2024/exercicio1.py
@@ -0,0 +1,8 @@
+#Exercício de fixação 01
+#Escreva um algoritmo que leia o valor de um número inteiro digitado pelo usuário e armazene esse valor em uma variável;
+#O algoritmo deve verificar se o valor digitado é um número positivo.
+#Se o valor digitado for maior do que ZERO o programa deve escrever na tela “O número é positivo!”
+
+n = int(input("Digite um número: "))
+if (n > 0):
+  print("O número é positivo!")
