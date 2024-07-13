1ec18524bd0a532f407f6c1b42009a3512a04629
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-07-13 17:41:59 -0300

    Lista de exercícios 02 adicionada

---
+++ 2024.1/ilp/exercicios/ilp-lista01-lucas-eduardo-e-igor-fabricio-main/lista01.py
@@ -0,0 +1,58 @@
+# 1. Calculadora Simples: Solicite ao usuário para inserir dois valores numéricos,
+# realize as operações de soma, subtração, multiplicação e divisão, e ao final exiba
+# os valores de cada uma das operações.
+
+
+# 2. Conversor de Temperatura: Solicite ao usuário um valor de temperatura em graus
+# Celsius, converta-a para Fahrenheit e exiba o resultado da conversão.
+
+
+# 3. Área do Círculo: Solicite ao usuário um valor do raio de um círculo, calcule sua
+# área e exiba o resultado do cálculo.
+
+
+# 4. Área do Triângulo: Solicite ao usuário um valor da base e da altura de um
+# triângulo, calcule sua área e exiba o resultado do cálculo.
+
+
+# 5. Volume da Esfera: Solicite ao usuário um valor do raio de uma esfera, calcule seu
+# volume e exiba o resultado do cálculo.
+
+
+# 6. Calculadora de Média Aritmética: Solicite ao usuário para que ele insira três
+# valores de notas, realize o cálculo da média aritmética e em seguida exiba os três
+# valores digitados pelo usuário e o resultado do cálculo.
+
+# n1 = float(input("Digite o valor da primeira nota: "))
+# n2 = float(input("Digite o valor da segunda nota: "))
+# n3 = float(input("Digite o valor da terceira nota: "))
+
+# ma = (n1+n2+n3)/3
+
+# print(f"Média aritimética = {ma:.2f} \n As notas foram: {n1}, {n2}, {n3}")
+
+
+# 7. Calculadora de Média Ponderada: Solicite ao usuário para que ele insira os
+# valores de 4 notas e seus respectivos pesos, em seguida realize o cálculo da média
+# pondera e exiba o resultado do cálculo.
+
+
+# 8. Equação de Segundo Grau: Solicite ao usuário os valores de “a”, “b”, “c” e “x”, em
+# seguida resolva uma equação quadrática do tipo y = ax2 + bx + c e exiba o valor
+# de y para o usuário.
+
+
+# 9. Calculadora de IMC: Solicite ao usuário os valores de peso (kg) e altura (m),
+# calcule o índice de massa corporal (IMC), sabendo que IMC = peso/altura², em seguida
+# exiba o valor do IMC calculado.
+
+
+# 10.Tabuada: Solicite ao usuário um valor numérico, em seguida, exiba a tabuada de
+# um número específico (por exemplo, 5). O programa deverá ter como saída:
+# 5x1 = 5; 5x2 = 10; 5x3 = 15; 5x4 = 20; 5x5 = 25; 5x6 = 30; 5x7 = 35; 5x8 = 40; 5x9
+# = 45; 5x10 = 50;
+
+
+# 11.Conversão de Segundos para o Formato HORA:MINUTO:SEGUNDO: Solicite
+# ao usuário um valor numérico correspondente à quantidade de segundos, em
+# seguida converta o valor para o formato de HORA:MINUTO:SEGUNDO.
