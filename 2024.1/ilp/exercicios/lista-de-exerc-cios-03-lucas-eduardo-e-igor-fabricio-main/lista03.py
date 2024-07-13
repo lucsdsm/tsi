ad7b5b513c7b692a51a46234db08d3cfcb6af5a6
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-07-13 17:44:35 -0300

    Lista de exercícios 03 adicionada

---
+++ 2024.1/ilp/exercicios/lista-de-exerc-cios-03-lucas-eduardo-e-igor-fabricio-main/lista03.py
@@ -0,0 +1,90 @@
+# 1. Escreva um algoritmo que imprima na tela do usuário os números inteiros de 1 até 100
+# (em ordem crescente).
+
+
+# 2. Escreva um algoritmo que imprima na tela do usuário os números inteiros de 1 até 100
+# (em ordem decrescente).
+
+
+# 3. Escreva um algoritmo que solicite ao usuário dois valores para determinação de um
+# intervalo. Ao final o algoritmo deverá imprimir todos os números desse intervalo e o
+# somatório deles.
+
+
+# 4. Escreva um algoritmo que solicite do usuário 10 números e ao final imprima na tela o
+# somatório desses números.
+
+
+# 5. Escreva um algoritmo que solicite do usuário 5 valores e afinal apresente na tela o
+# somatório dos valores menores que 10.
+
+
+# 6. Escreva um algoritmo que solicite do usuário 5 valores e afinal apresente na tela o
+# somatório dos valores maiores ou igual a 10 e menor do que 20.
+
+
+# 7. Escreva um algoritmo que solicite do usuário 5 valores e afinal apresente na tela o
+# somatório dos valores pares que foram digitados.
+
+
+# 8. Escreva um algoritmo que solicite do usuário um número correspondente à quantidade
+# de valores que o usuário fornecerá para o algoritmo. Ao final, o algoritmo deverá infor-
+# mar quantos números pares foram digitados.
+
+# 9. Escreva um algoritmo que solicite do usuário 10 valores. O algoritmo deverá calcular
+# a soma da sequência de valores pares e dos valores ímpares, ou seja, somar o 1o
+# número digitado com o 3o, 5o, 7o e 9o e o mesmo para os números pares. Após, informar
+# se o somatório dos números ímpares é maior, igual ou menor do que o dos números
+# pares.
+
+
+# 10. Escreva um algoritmo que solicite do usuário 10 valores inteiros. O algoritmo deverá
+# calcular o somatório dos números pares e dos números ímpares que forem digitados
+# pelo usuário. Após o somatório, o algoritmo deve informar se o somatório dos números
+# ímpares é maior, igual ou menor do que o dos números pares.
+
+# 11. Escreva um algoritmo que informe ao usuário que calcula o somatório de uma sequên-
+# cia de números. O algoritmo deverá solicitar ao usuário o total de números que deverão
+# ser somados. Depois o algoritmo deve realizar a soma de todos os números e apre-
+# sentar na tela o resultado dessa soma conforme exemplo abaixo:
+# Digite o total de números a serem somados: 5
+# 2 7 3 8 6 (números digitados pelo usuário)
+# Saída no terminal: 2+7+3+8+6=26
+
+
+# 12. Escreva um algoritmo que imprima na tela todos os números divisíveis por 7 (separa-
+# dos por “;”), mas que não sejam múltiplos de 5 no intervalo de 1000 a 3000.
+
+
+# 13. Escreva um algoritmo que imprima na tela a tabuada da multiplicação de um número
+# inteiro de 1 a 10. Exemplo:
+# Entrada: 9
+# Saída: 1x9=9; 2x9=18; 3x9=27; 4x9=36; 5x9=45; 6x9=54; 7x9=63; 8x9=72;
+# 9x9=81; 10x9=90;
+
+
+# 14. Escreva um algoritmo que solicite ao usuário um número inteiro e depois imprima uma
+# sequência de caracteres que represente um triângulo. Exemplo:
+# Entrada: 3 2
+# Saída:
+# *
+# * *
+# * * *
+# * *
+# *
+
+# *
+# * *
+# *
+
+
+# 15. Escreva um algoritmo que solicite números ao usuário e conte quantos desses são
+# pares e quantos são ímpares, até que seja digitado um número negativo. Ao final im-
+# prima na tela quantos números pares e ímpares foram digitados.
+
+
+# 16. Escreva um algoritmo que solicite ao usuário um valor e em seguida apresente na tela
+# uma sequência começando em 1 e indo até o valor fornecido pelo usuário. Porém, se
+# nessa sequência houver um número que seja múltiplo de 3 escreva PI, e se houver
+# um número que seja múltiplo de 7 escreva PA. Caso haja um número que seja múltiplo
+# de 3 e 7 escreva POW.
