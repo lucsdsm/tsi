9b05a35d8e47c2d21c82419bd7c8f790d2f58821
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-07-14 17:08:45 -0300

    questão 1 da lista de exercícios 3 adicionada

--- 2024.1/ilp/exercicios/lista-de-exerc-cios-03-lucas-eduardo-e-igor-fabricio-main/lista03.py
+++ 2024.1/ilp/exercicios/lista-de-exerc-cios-03-lucas-eduardo-e-igor-fabricio-main/lista03.py
@@ -1,6 +1,9 @@
 # 1. Escreva um algoritmo que imprima na tela do usuário os números inteiros de 1 até 100
 # (em ordem crescente).
 
+# for x in range(1, 101):
+#     print(x)
+
 
 # 2. Escreva um algoritmo que imprima na tela do usuário os números inteiros de 1 até 100
 # (em ordem decrescente).
