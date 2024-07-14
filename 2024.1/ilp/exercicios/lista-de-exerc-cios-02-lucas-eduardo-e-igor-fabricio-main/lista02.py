e7305e615187a6fb7e621acff4fc58f5c743521c
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-07-14 10:54:59 -0300

    questão 2 da lista de exercícios 2 adicionada

--- 2024.1/ilp/exercicios/lista-de-exerc-cios-02-lucas-eduardo-e-igor-fabricio-main/lista02.py
+++ 2024.1/ilp/exercicios/lista-de-exerc-cios-02-lucas-eduardo-e-igor-fabricio-main/lista02.py
@@ -41,6 +41,14 @@
 # mas não por 100. Para que um número X seja considerado divisível por um número
 # Y é preciso que o resto da divisão de X por Y seja igual a ZERO].
 
+# ano = int(input("Ano: "))
+
+# if (ano % 4 == 0) and (ano % 100 != 0):
+#     print(f"{ano} é um ano bissexto.")
+
+# else:
+#     print(f"{ano} não é um ano bissexto.")
+
 
 # 3. Escreva um programa que solicite um número entre 1 e 10. Caso o usuário digite um
 # valor dentro dessa faixa o programa deverá exibir a mensagem “O número digitado
