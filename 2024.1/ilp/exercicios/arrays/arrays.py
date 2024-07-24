c5429a3b56ef1ed189fe7631a107af9218a442c8
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-07-24 11:52:56 -0300

    adicionada lista de arrays

---
+++ 2024.1/ilp/exercicios/arrays/arrays.py
@@ -0,0 +1,27 @@
+# 1. Escreva um programa que leia uma lista de números inteiros fornecida
+# pelo usuário e remova todos os elementos duplicados, mantendo a
+# ordem original dos elementos. Ao final, apresente os valores fornecidos
+# pelo usuário e o resultado do processamento da sua solução;
+
+# 2. Escreva um programa que leia uma lista de números inteiros fornecida
+# pelo usuário e um número alvo. O programa deve encontrar todos os
+# pares de números na lista cuja soma seja igual ao número alvo.
+# 1. Exemplo: Digite os números separados por espaço: 2 4 3 5 7
+
+# Digite o valor alvo: 7
+# Pares encontrados: (2, 5), (3, 4)
+
+# 3. Escreva um programa que leia uma lista de números inteiros fornecida pelo
+# usuário e um número de passos. O programa deve rotacionar a lista para a
+# direita pelo número de passos especificado.
+# Exemplo: Digite os números separados por espaço: 1 2 3 4 5
+
+# Digite o número de passos: 2
+# Lista rotacionada: [4, 5, 1, 2, 3]
+
+# 4. Escreva um programa que leia uma lista de números inteiros fornecida pelo
+# usuário e utilize uma list comprehension para gerar uma nova lista contendo
+# os quadrados de todos os números ímpares da lista original.
+# Exemplo: Digite os números separados por espaço: 1 2 3 4 5 6 7 8 9 10
+
+# Quadrados dos números ímpares: [1, 9, 25, 49, 81]
