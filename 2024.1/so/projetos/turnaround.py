1e7d2bf1e7d5339b244bcead82d12f442861ce02
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-08-06 06:29:08 -0300

    implementado algoritmo SFJ

--- 2024.1/so/projetos/turnaround.py
+++ 2024.1/so/projetos/turnaround.py
@@ -4,6 +4,7 @@ if (algoritmo == "Fifo" or algoritmo == "SJF"):
     quantidade_processos = int(input("Quantidade de processos para serem simulados: "))
     lista_tempos = []
     lista_tempos_turnaround = []
+    media = 0
 
     # Digitar os tempos dos processos de acordo com a quantidade de processos.
     for x in range(quantidade_processos):
@@ -11,19 +12,28 @@ if (algoritmo == "Fifo" or algoritmo == "SJF"):
         novo_tempo = [tempo_processo] # Transforma a variável em uma lista.
         lista_tempos = lista_tempos + novo_tempo # Concatena a variável "novo_tempo" com a lista de tempos.
 
+    # Fifo
     if algoritmo == "Fifo":
         # Adiciona à lista de tempos de turnaround levando em consideração apenas os tempos de cada processo com o tempo do processo anterior.
         tempo_turnaround = 0
-        for x in range(len(lista_tempos)):
-            tempo_turnaround = tempo_turnaround + lista_tempos[x] # Adiciona o valor de turnaround ao valor de X na lista de tempos.
-            novo_tempo_turnaround = [tempo_turnaround] # Transforma o novo tempo de turnaround em uma lista.
-            lista_tempos_turnaround = lista_tempos_turnaround + novo_tempo_turnaround # Concatena a variável "novo_tempo_turnaround" com a lista de tempos de turnaround.
+        for x in lista_tempos:
+            tempo_turnaround = tempo_turnaround + x # Adiciona o valor de turnaround ao valor de x.
+            lista_tempos_turnaround = lista_tempos_turnaround + [tempo_turnaround] # Concatena a variável "lista_tempos_turnaround" com a variável "tempo_turnaround".
 
-        print(f"Seguindo o algoritmo {algoritmo}, os tempos de turnaround de cada processo são: \n{lista_tempos_turnaround}")
+        # Calcular média
+        for x in lista_tempos_turnaround:
+            media = media + x
+        
+        media = media / quantidade_processos
+
+        print(f"Seguindo o algoritmo {algoritmo}, os tempos de turnaround de cada processo são: \n{lista_tempos_turnaround}\nA média de tempo para cada processo será de {media:.2f} segundos.")
     
+    # SJF
     elif algoritmo == "SJF":
 
-        # lista_tempos_ordenados = []
+        # Ordenar lista sem o .sort()
+
+        # lista_tempos_ordenados = [] 
         # for x in lista_tempos:
         #     for y, valor in enumerate(lista_tempos_ordenados):
         #         if x < valor:
@@ -37,10 +47,22 @@ if (algoritmo == "Fifo" or algoritmo == "SJF"):
         for x in lista_tempos:
             turnaround = x
             for y in lista_tempos:
-                    if x <= y:
-                        pass
-                    elif x > y:
-                        pass
+                if x <= y:
+                    pass
+    
+                elif x > y:
+                    turnaround = turnaround + y
+                    print(f"{x} > {y}; turnaround = {turnaround}")
+                    
+            lista_tempos_turnaround = lista_tempos_turnaround + [turnaround]   
+
+        # Calcular média
+        for x in lista_tempos_turnaround:
+            media = media + x
+        
+        media = media / quantidade_processos 
+
+        print(f"Seguindo o algoritmo {algoritmo}, os tempos de turnaround de cada processo são: \n{lista_tempos_turnaround}\nA média de tempo para cada processo será de {media:.2f} segundos.")
 else:
     print(f"'{algoritmo}' não é um algoritmo válido (Fifo/SJF). Digite um algoritmo válido.")
         
