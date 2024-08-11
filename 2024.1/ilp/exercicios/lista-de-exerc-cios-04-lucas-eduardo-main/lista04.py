53137d8ffca37a92eb98fabf946195db9b2173d3
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-08-11 06:17:29 -0300

    adicionada lista4 de ilp

---
+++ 2024.1/ilp/exercicios/lista-de-exerc-cios-04-lucas-eduardo-main/lista04.py
@@ -0,0 +1,110 @@
+# Construa soluções de algoritmos e implemente-os utilizando a linguagem de programação
+# Python para os seguintes casos:
+# OSBERVAÇÃO:
+# Exemplos no pdf da lista;
+# As soluções apresentadas NÃO devem usar as funções buit-in das listas em Python;
+# As soluções apresentadas NÃO devem usar funções definidas pelo usuário;
+# As saídas das soluções devem seguir o formato estabelecido nos exemplos,
+# quando demonstrado.
+
+# VETORES (array unidimensional):
+
+# 1. Escreva um programa que leia 10 valores inteiros e armazene em uma lista. O pro-
+# grama deve imprimir no terminal quantos valores pares foram digitados pelo usuário.
+# Dica: use o operador “%” para verificar se o número é par (ZERO é neutro, ZERO NÃO
+# É PAR).
+
+# 2. Escreva um programa que recebe como entrada valores inteiros para criar duas listas
+# de mesmo tamanho. O tamanho das listas deverá ser definido pelo usuário. O pro-
+# grama deverá somar os valores pares e ímpar de cada uma das listas. Em seguida,
+# comparar as somas e informar qual dos somatórios é maior ou se há um empate.
+
+# 3. leia 10 valores inteiros e armazene em uma lista. O programa deve imprimir no terminal
+# quantos valores pares foram digitados pelo usuário. Dica: use o operador “%” para
+# verificar se o número é par (ZERO é neutro, ZERO NÃO É PAR).
+
+# 4. Escreva um programa que receba como entrada 3 parâmetros: um valor inteiro corres-
+# pondente à quantidade de elementos de dois arrays unidimensionais; duas sequências
+# de valores do tamanho do primeiro parâmetro, os quais deverão ser armazenados em
+# duas listas distintas. Em seguida, o programa deverá criar uma lista resultante formada
+# pela intercalação dos valores de cada uma das sequências de entrada. Como saída o
+# programa deverá imprimir as duas listas iniciais e a lista resultante.
+
+# 5. Escreva um programa que receba como entrada uma sequência de valores inteiros.
+# Para tanto, o programa deverá inicialmente solicitar ao usuário quantos valores serão
+# fornecidos para análise e só depois solicitar os valores a serem analisados. A análise
+# consistirá em identificar e apresentar a partir da sequência de valores fornecidos, o
+# menor valor, o maior valor e a média aritmética dos valores.
+
+# 6. Crie um programa que solicite ao usuário uma lista de números inteiros e uma string
+# de mesmo comprimento. O programa deve substituir os números nos índices ímpares
+# da lista por caracteres correspondentes da string nos mesmos índices. Exiba a se-
+# quência resultante, separada por um espaço em branco.
+
+# 7. Para tanto, o programa deverá inicialmente solicitar ao usuário quantos valores serão
+# fornecidos para análise e só depois solicitar os valores a serem analisados. A análise
+# consistirá em identificar e apresentar a partir da sequência de valores fornecidos, o
+# menor valor, o maior valor e a média aritmética dos valores. 
+
+# 8. Escreva um programa que receba como entrada uma string constituída por uma se-
+# quência de números inteiros separados por espaço. O programa deverá transformar
+# essa string em uma lista de números inteiros e apresentar o resultado da soma dos
+# valores das posições ímpares dessa lista.
+
+# 9. Escreva um programa que receba como entrada uma string várias palavras separadas
+# por espaço. O programa deverá verificar e apresentar a quantidade de ocorrência de
+# cada palavra no texto repassado como entrada para o programa. Os sinais de pontu-
+# ação não devem ser contabilizados, como por exemplo “.” Ou “,”.
+
+
+# MATRIZES (array bidimensional):
+# 10. Escreva um programa que leia 9 valores inteiros e armazene em uma matriz 3x3. O
+# programa deve informar quantos valores ímpares foram digitados pelo usuário e impri-
+# mir a matriz no formato 3x3. Dica: use o operador “%” para verificar se o número é
+# ímpar.
+
+# 11. Escreva um programa que solicite ao usuário o valor das duas dimensões de uma
+# matriz. Em seguida, solicite ao usuário e armazene nessa matriz uma quantidade de
+# valores inteiros correspondente ao tamanho necessário para preencher todas as posi-
+# ções da matriz. O programa deverá exibir a matriz de dimensões “m por n” informar o
+# resultado da soma de cada LINHA da matriz. Exemplos:
+
+# 12. Escreva um programa que solicite ao usuário o valor das duas dimensões de uma
+# matriz. Em seguida, solicite ao usuário e armazene nessa matriz uma quantidade de
+# valores inteiros correspondente ao tamanho necessário para preencher todas as posi-
+# ções da matriz. O programa deverá exibir a matriz de dimensões “m por n” informar o
+# resultado da soma de cada COLUNA da matriz. Exemplos:
+
+# 13. Escreva um programa que armazene valores inteiros em duas matrizes A e B de tama-
+# nho 3x3. Em seguida gere uma terceira matriz C 3x3 formada pelos maiores valores
+# de cada posição comparando as duas primeiras matrizes A e B. 
+
+# 14. Escreva um programa que armazene valores inteiros em uma matrize de tamanho
+# 4x4. Em seguida exiba a soma dos elementos que estão em índices de linha ímpar e
+# coluna par.
+
+# 15. Escreva um programa que, dada uma matriz de números inteiros aleatórios variando
+# entre 100 e 999 de dimensões ‘m’ por ‘n’, ambos podendo variar de 2 a 10, realize os
+# seguintes passos: solicite ao usuário as dimensões da matriz; apresente a matriz ge-
+# rada de forma aleatória; informe o valor e posição do maior e menor valor na matriz.
+
+
+# 16. Escreva um programa que realiza o produto matricial entre duas matrizes de dimen-
+# sões 3x3. Os valores das duas matrizes devem ser gerados de forma aleatória com
+# números inteiros aleatórios variando entre 1 e 9. Ao final, apresente as duas matrizes
+# geradas de forma aleatória, bem como a matriz resultante do produto. 
+
+# 17. Escreva um programa que realiza o produto matricial entre duas matrizes de dimen-
+# sões ‘j’ por ‘k’ e ‘m’ por ‘n’ fornecidas pelo usuário. Os valores das duas matrizes devem
+
+# ser gerados de forma aleatória com números inteiros aleatórios variando entre dois
+# valores, também fornecidos pelo usuário. Ao final, apresente as duas matrizes geradas
+# de forma aleatória, bem como a matriz resultante do produto. Caso não seja possível
+# realizar o produto matricial, informe a impossibilidade ao usuário e as duas matrizes
+# geradas.
+
+# 18. Escreva um programa que leia uma matriz de dimensões 3x6 com valores reais. Em
+# seguida, o programa deverá imprimir a soma de todos os elementos das colunas ím-
+# pares; imprima a média aritmética dos elementos da segunda e quarta colunas; subs-
+# titua os valores da sexta coluna pela soma dos valores das colunas 4 e 5; Exiba a
+# matriz modificada e a matriz original.
