85779f4dc115e26156f11a15ed62f5b4184af98e
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-08-21 11:34:47 -0300

    adicionados exercícios da lista da aula 13 de ilp

---
+++ 2024.1/ilp/exercicios/exerc-cios-aula-13-lucas_eduardo/exercicios.py
@@ -0,0 +1,40 @@
+# Tuplas:
+
+# 1. Escreva um programa que receba uma tupla de números inteiros e como
+# saída uma nova tupla com os elementos pares da tupla original. Por
+# exemplo, se a tupla for (1, 2, 3, 4, 5), o programa deve imprimir (2, 4).
+
+
+# 2. Escreva um programa que receba uma tupla de strings e exiba uma nova
+# tupla com as strings ordenadas alfabeticamente e sem repetições. Por
+# exemplo, se a tupla for ("banana", "maçã", "laranja", "banana", "uva"), o
+# programa deve imprimir ("banana", "laranja", "maçã", "uva").
+
+# Dicionários:
+
+# 3. Escreva um programa que receba um dicionário que mapeia nomes de
+# países para suas populações e exiba o nome do país com a maior
+# população. Por exemplo, se o dicionário for {"Brasil": 211.8, "China":
+# 1400.5, "Índia": 1366.4}, o programa deve exibir "China".
+
+
+# 4. Escreva um programa que receba como entrada um dicionário que mapeia
+# nomes de alunos para suas notas e exiba um novo dicionário com os nomes
+# dos alunos aprovados e suas respectivas médias. Considere que um aluno é
+# aprovado se sua média for maior ou igual a 7. Por exemplo, se o dicionário
+# for {"Ana": [8.5, 9.0, 7.5], "Bruno": [6.0, 5.5, 4.0], "Carla": [7.0, 8.0, 9.0]},
+# o programa deve exibir {"Ana": 8.33, "Carla": 8.0}.
+
+# Conjuntos:
+
+# 5. Escreva um programa que receba dois conjuntos de números inteiros como
+# entrada e exiba um novo conjunto com os elementos que estão em ambos
+# os conjuntos. Por exemplo, se os conjuntos forem {1, 2, 3, 4} e {3, 4, 5, 6},
+# o programa deve exibir {3, 4}.
+
+
+# 6. Escreva um programa que receba um conjunto de strings e exiba um novo
+# conjunto com as strings que são palíndromos. Um palíndromo é uma palavra
+# que é igual a ela mesma quando lida de trás para frente. Por exemplo, se o
+# conjunto for {"arara", "casa", "ovo", "radar"}, o programa deve exibir
+# {"arara", "ovo", "radar"}.
