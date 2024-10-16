3adc1fb300c0dca13a41d993b5c53189d2b74b38
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-10-16 11:49:23 -0300

    arquivo da aula 2 de dwfe

--- 2024.2/dwfe/Aula 2/aula2.ts
+++ 2024.2/dwfe/Aula 2/aula2.ts
@@ -16,7 +16,7 @@ class Aluno {
     nomeNVezes(vezes: Number) : string {
         let result : string = "";
 
-        for(let index = 0, index <= vezes; index++) {
+        for(let index = 0, index <= vezes; index + 1) {
             
         }
 
