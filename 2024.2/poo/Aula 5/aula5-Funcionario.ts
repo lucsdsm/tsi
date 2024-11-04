2e5c7be42b5dbcdf48eb8eff2733cc344d888773
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-11-04 10:08:37 -0300

    adicionada resolução parcial da questão de exercícios da aula5 de poo

---
+++ 2024.2/poo/Aula 5/aula5-Funcionario.ts
@@ -0,0 +1,39 @@
+// Crie uma classe base Funcionario:
+// Propriedades:
+// nome (string)
+// salarioBase (number)
+
+// Construtor: inicialize as propriedades nome e salarioBase.
+
+// Método:
+// calcularSalario() que retorna o salário base.
+
+// Crie uma classe Gerente que herda de Funcionario:
+// Adicione uma nova propriedade bonus (number).
+// Modifique o método calcularSalario() para retornar o salário base somado ao bônus.
+
+// Crie uma classe Vendedor que herda de Funcionario:
+
+// Adicione uma nova propriedade comissao (number).
+// Modifique o método calcularSalario() para retornar o salário base somado à comissão.
+
+// Teste o código:
+// Crie uma instância de Funcionario, Gerente e Vendedor.
+// Exiba o salário de cada um usando o método calcularSalario.
+
+class Funcionario{
+    nome: string;
+    salarioBase: number;
+   
+    constructor(
+      nome: string,
+      salarioBase: number) {
+      this.nome = nome;
+      this.salarioBase = salarioBase;
+    }
+   
+    calcularSalario() {
+      return this.salarioBase;
+    }
+}
+
