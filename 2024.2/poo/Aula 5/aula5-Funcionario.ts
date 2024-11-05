f086e1d363ea56bf5d610ed7e8c0c5ad8045fbcd
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-11-04 22:00:03 -0300

    resolução parcial da atividade de aula5 de poo

--- 2024.2/poo/Aula 5/aula5-Funcionario.ts
+++ 2024.2/poo/Aula 5/aula5-Funcionario.ts
@@ -21,19 +21,47 @@
 // Crie uma instância de Funcionario, Gerente e Vendedor.
 // Exiba o salário de cada um usando o método calcularSalario.
 
-class Funcionario{
-    nome: string;
-    salarioBase: number;
-   
-    constructor(
-      nome: string,
-      salarioBase: number) {
-      this.nome = nome;
-      this.salarioBase = salarioBase;
-    }
-   
-    calcularSalario() {
-      return this.salarioBase;
-    }
+class Funcionario {
+  nome: string;
+  salarioBase: number;
+ 
+  constructor(nome: string, salarioBase: number) {
+    this.nome = nome;
+    this.salarioBase = salarioBase;
+  }
+ 
+  calcularSalario() {
+     return this.salarioBase;
+  }
 }
+
+class Gerente extends Funcionario {
+bonus: number;
+  
+  constructor(nome: string, salarioBase: number, bonus: number) {
+  super(nome, salarioBase);
+      this.bonus = bonus;
+  }
+  
+  calcularSalario() {
+     return this.salarioBase + this.bonus;
+  }
+}
+
+class Vendedor extends Funcionario {
+comissao: number;
+  
+  constructor(nome: string, salarioBase: number, comissao: number) {
+  super(nome, salarioBase);
+      this.comissao = comissao;
+  }
+  
+  calcularSalario() {
+     return this.salarioBase + this.comissao;
+  }
+}
+
+let f: Funcionario = new Funcionario("Lucas", 1200);
+f.calcularSalario();
+
 
