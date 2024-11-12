85059a8519870d5ab5920a68004760fd44d66da1
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-11-12 10:07:01 -0300

    alterados arquivos de exercícios da aula6 de poo

--- 2024.2/poo/aula6/cenario1.ts
+++ 2024.2/poo/aula6/cenario1.ts
@@ -1,31 +1,3 @@
-// Cenário
-// • Em uma empresa, existem Funcionários que trabalham em diferentes
-// Departamentos. Cada funcionário pertence a um departamento específico, mas um
-// departamento pode ter vários funcionários.
-
-// • Instruções
-// 1. Crie uma classe chamada Departamento, que possui:
-// o Um atributo nome (do tipo string), que representa o nome do
-// departamento.
-// o Um método exibirInformacoes que mostra no console o nome do
-// departamento.
-
-// 2. Crie uma classe chamada Funcionario, que possui:
-// o Um atributo nome (do tipo string), que representa o nome do
-// funcionário.
-// o Um atributo departamento do tipo Departamento para associar o
-// funcionário ao seu departamento.
-// o Um método exibirInformacoes que mostra no console o nome do
-// funcionário e o nome do seu departamento.
-
-// 3. No código principal, faça o seguinte:
-// o Crie duas instâncias de Departamento chamadas departamento1 e
-// departamento2, com nomes como "TI" e "RH".
-// o Crie três instâncias de Funcionario e associe cada uma a um
-// departamento.
-// o Chame o método exibirInformacoes de cada Funcionario para exibir as
-// informações no console.
-
 class Departamento {
   nome: string;
 
@@ -34,7 +6,7 @@ class Departamento {
   }
 
   exibirInformacoes() {
-    return "Departamento: " + this.nome;
+    return this.nome
   }
 }
 
@@ -48,7 +20,7 @@ class Funcionario {
   }
 
   exibirInformacoes() {
-    return "Funcionário: " + this.nome + "; " + this.departamento.nome
+    return this.nome
   }
  
 }
@@ -60,6 +32,6 @@ let funcionario1 = new Funcionario("Alice", departamento1);
 let funcionario2 = new Funcionario("Bruno", departamento1);
 let funcionario3 = new Funcionario("Carla", departamento2);
 
-console.log(funcionario1.exibirInformacoes())
-console.log(funcionario2.exibirInformacoes())
-console.log(funcionario3.exibirInformacoes())
+console.log(`Funcionário: ${funcionario1.exibirInformacoes()}, Departamento: ${funcionario1.departamento.exibirInformacoes()}`)
+console.log(`Funcionário: ${funcionario2.exibirInformacoes()}, Departamento: ${funcionario2.departamento.exibirInformacoes()}`)
+console.log(`Funcionário: ${funcionario3.exibirInformacoes()}, Departamento: ${funcionario3.departamento.exibirInformacoes()}`)
