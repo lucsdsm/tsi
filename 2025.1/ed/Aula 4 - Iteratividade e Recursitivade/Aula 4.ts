49893bdda30a3336c059d3ee886461c3f788bbfc
Lucas Eduardo <lucaseduardo168@gmail.com>
2025-04-28 10:19:51 -0300

    aula 4 de ed

--- 2025.1/ed/Aula 4 - Iteratividade e Recursitivade/Aula 4.ts
+++ 2025.1/ed/Aula 4 - Iteratividade e Recursitivade/Aula 4.ts
@@ -24,11 +24,18 @@ function fibonacci(n: number): number {
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+console.log(`Fibonacci: ${fibonacci(5)}`); 
+
+// 1
 function somaVetor(vetor: number[], n: number): number {
     if (n < 0) return 0;
     return vetor[n] + somaVetor(vetor, n - 1);
 }
 
+const v = [1, 2, 3, 4, 5];
+console.log(`Soma do vetor: ${somaVetor(v, v.length - 1)}`);
+
+// 2
 function contagemRegressiva(n: number): number {
     if (n == 1) {
         return 1;
@@ -40,5 +47,36 @@ function contagemRegressiva(n: number): number {
 }
 
 console.log(contagemRegressiva(5)); // 5 4 3 2 1
-const v = [1, 2, 3, 4, 5];
-console.log(`Soma do vetor: ${somaVetor(v, v.length - 1)}`);
+
+// 3
+function potencia(base: number, expoente: number): number {
+    if (expoente == 0) return 1;
+    return base * potencia(base, expoente - 1);
+}
+
+console.log(`Potência: ${potencia(2, 3)}`); // 8
+
+// 4
+function mdc(a: number, b: number): number {
+    if (b == 0) return a;
+    return mdc(b, a % b);
+}
+
+console.log(`MDC: ${mdc(48, 18)}`); // 
+
+
+// 5
+function multiplicacao(a: number, b: number): number {
+    if (b == 0) return 0;
+    return a + multiplicacao(a, b - 1);
+}
+
+console.log(`Multiplicação: ${multiplicacao(3, 4)}`); // 12
+
+// 6
+function converteNaturalBinario(n: number): string {
+    if (n == 0) return "";
+    return converteNaturalBinario(Math.floor(n / 2)) + (n % 2);
+}
+
+console.log(`Número binário: ${converteNaturalBinario(2)}`); // 10  
