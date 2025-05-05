99f78a7d8a8da96201b3c6d0f9c7e311ecb36058
Lucas Eduardo <lucaseduardo168@gmail.com>
2025-05-05 07:50:16 -0300

    finalização da atividade da aula3 de dwbe

--- 2025.1/dwbe/Aula 3 - Exercício com templates/aula3.py
+++ 2025.1/dwbe/Aula 3 - Exercício com templates/aula3.py
@@ -42,18 +42,47 @@ def home():
                 return redirect(url_for('home', person=usuario))
             else:
                 return render_template('index.html', invalido=True)
-        
+
         # Caso contrário, é ação relacionada a música
         person = request.args.get('person')
+        
         if person in musicas_favoritas:
             acao = request.form.get('acao')
             musica = request.form.get('musica', '').strip()
+
             if acao == 'add' and musica:
                 musicas_favoritas[person].append(musica)
+
             elif acao == 'remove' and musica in musicas_favoritas[person]:
                 musicas_favoritas[person].remove(musica)
 
+            elif acao == 'editar':
+                # Modo de edição: mostrar o campo de edição
+                lista_completa = musicas_favoritas.get(person, [])
+                return render_template(
+                    'home.html',
+                    person=person,
+                    musicas=lista_completa,
+                    busca='',
+                    musica_editar=musica
+                )
+
+            elif acao == 'editar_salvar':
+                original = request.form.get('original', '').strip()
+                novo_nome = request.form.get('novo_nome', '').strip()
+                if original in musicas_favoritas[person] and novo_nome:
+                    index = musicas_favoritas[person].index(original)
+                    musicas_favoritas[person][index] = novo_nome
+
     # GET ou após redirecionamento POST
     person = request.args.get('person')
-    lista = musicas_favoritas.get(person, [])
-    return render_template('home.html', person=person, musicas=lista)
+    busca = request.args.get('busca', '').strip().lower()
+    lista_completa = musicas_favoritas.get(person, [])
+
+    if busca:
+        lista = [m for m in lista_completa if busca in m.lower()]
+    else:
+        lista = lista_completa
+
+    return render_template('home.html', person=person, musicas=lista, busca=busca, musica_editar=None)
+
